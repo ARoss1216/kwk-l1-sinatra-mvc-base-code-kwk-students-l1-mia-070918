23c7278c5939c209f37b32cebf32cc45f83558b2
ARoss1216 <a.ross24852@gmail.com>
2018-07-18 14:56:31 +0000

    automatically backed up by learn

---
+++ models/model.rb
@@ -0,0 +1,5 @@
+def get_fortune
+  fortune_array = ["you will prosper", "you will win the vegetable off today", "you will get to meet Karlie Kloss", "you will become the greatest computer scientist of all time", "your crush will fall in love with you"]
+  return fortune_array.sample
+end
+
